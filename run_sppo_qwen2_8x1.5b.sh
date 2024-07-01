a8bc87967a2c86dd12992e252944c0099c23a9e7
shootime2021 <shootime007@163.com>
2024-07-01 13:33:55 +0800

    add mode update

--- run_sppo_qwen2_8x1.5b.sh
+++ run_sppo_qwen2_8x1.5b.sh
@@ -6,10 +6,18 @@ for i in $(seq 1 $iter_num); do
     else
         MODEL=$OUTPUT_DIR
     fi
-    OUTPUT_DIR="xDAN2099/xDAN-L1-qwen2_moe-8x1.5b-Iter${i}"
+    OUTPUT_DIR="xDAN2099/xDAN-L1-qwen2_moe-8x1.5b-Instruct-0701-Iter${i}"
     PROMPT="UCLA-AGI/data-mistral-7b-instruct-sppo-iter${i}"
-    OUT="data-xDAN-L1-qwen2_moe-8x1.5b-iter${i}"
+    OUT="data-gemma-2-27b-it-sppo-iter${i}"
+    echo "runing epoch $i"
+    DATASET="synthetic_data_xDAN-L1-qwen2_moe-8x1.5b-sppo-iter${i}_score"
 
-    bash scripts/generate.sh --model $MODEL --prompt $PROMPT --out_path $OUT
-    bash scripts/pipeline.sh --model $MODEL --iter $i --dataset "synthetic_data_xDAN-L1-qwen2_moe-8x1.5b-sppo-iter${i}_score" --output_dir $OUTPUT_DIR --num 1
+    if [ ! -d "$DATASET" ]; then
+        bash scripts/generate.sh --model $MODEL --prompt $PROMPT --out_path $OUT
+    fi
+    bash scripts/pipeline.sh --model $MODEL --iter $i --dataset "$DATASET" --output_dir $OUTPUT_DIR --num 1 --batch_size 1 --accumulate 8
 done
+
+
+
+        
