f0d9b243d327e7e748c179210d14045998da51d7
shootime2021 <shootime007@163.com>
2024-07-01 13:21:56 +0800

    xDAN2099

--- run_sppo_qwen2_8x1.5b.sh
+++ run_sppo_qwen2_8x1.5b.sh
@@ -6,7 +6,7 @@ for i in $(seq 1 $iter_num); do
     else
         MODEL=$OUTPUT_DIR
     fi
-    OUTPUT_DIR="checkpoints/xDAN-L1-qwen2_moe-8x1.5b-Iter${i}"
+    OUTPUT_DIR="xDAN2099/xDAN-L1-qwen2_moe-8x1.5b-Iter${i}"
     PROMPT="UCLA-AGI/data-mistral-7b-instruct-sppo-iter${i}"
     OUT="data-xDAN-L1-qwen2_moe-8x1.5b-iter${i}"
 
